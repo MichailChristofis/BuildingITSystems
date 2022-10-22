300d7e9602f8900d693097f90c432985c509eccd
s3923089 <90187172+MichailChristofis@users.noreply.github.com>
2022-10-22 23:27:56 +0300

    home page responsive to large screen size

--- pages/recipe.php
+++ pages/recipe.php
@@ -158,53 +158,55 @@
     </main>
   </body>
   <footer>
-    <div class="flex spacebetween">
-      <div class="contactw">
-        <span class="colw popbo consi">Contact Us</span>
-        <div class="flex spacebetween mato mabo">
-          <span class="colw popno emsi">Email:</span>
-          <input type="text" class="email">
+    <div class="footerdiv">
+      <div class="flex spacebetween">
+        <div class="contactw">
+          <span class="colw popbo consi">Contact Us</span>
+          <div class="flex spacebetween mato mabo">
+            <span class="colw popno emsi">Email:</span>
+            <input type="text" class="email">
+          </div>
+          <div class="flex spacebetween mabo">
+            <span class="colw popno emsi">Subject:</span>
+            <input type="text" class="subject">
+          </div>
+          <div class="message">
+            <span class="bl colw popno mabo emsi">Message:</span>
+            <textarea name="message" id="mes" class="bl" cols="35" rows="6" maxlength="256"></textarea>
+            <span class="meschar">
+              0/256
+            </span>
+            <button type="submit">
+              <img src="./../assets/plane.svg" alt="plane image">
+            </button>
+          </div>
         </div>
-        <div class="flex spacebetween mabo">
-          <span class="colw popno emsi">Subject:</span>
-          <input type="text" class="subject">
-        </div>
-        <div class="message">
-          <span class="bl colw popno mabo emsi">Message:</span>
-          <textarea name="message" id="mes" class="bl" cols="35" rows="6" maxlength="256"></textarea>
-          <span class="meschar">
-            0/256
-          </span>
-          <button type="submit">
-            <img src="./../assets/plane.svg" alt="plane image">
-          </button>
-        </div>
-      </div>
-      <div>
-        <span class="colw popbo consi">Sitemap</span>
         <div>
-          <span class="bl colw popno emsi mato mabo">Home</span>
-          <span class="bl colw popno emsi mabo">My Profile</span>
-          <span class="bl colw popno emsi mabo">About Us</span>
-          <span class="bl colw popno emsi mabo">Privacy Policy</span>
+          <span class="colw popbo consi">Sitemap</span>
+          <div>
+            <span class="bl colw popno emsi mato mabo">Home</span>
+            <span class="bl colw popno emsi mabo">My Profile</span>
+            <span class="bl colw popno emsi mabo">About Us</span>
+            <span class="bl colw popno emsi mabo">Privacy Policy</span>
+          </div>
         </div>
-      </div>
-      <div>
-        <img src="./../assets/logo.png" class="log" alt="Logo">
-        <div class="flex aligncenter">
-          <img src="./../assets/email.png" class="emicon" alt="email icon">
-          <span class="colw popno emsi emtext">support@recipewise.com</span>
-        </div>
-        <div class="flex aligncenter contacttop">
-          <img src="./../assets/phone.png" class="phicon" alt="phone icon">
-          <span class="colw popno emsi">1300 566 466</span>
-        </div>
-        <div class="socialdiv flex spacebetween">
-          <img class="socialim" src="./../assets/facebook.png" alt="Facebook image">
-          <img class="socialim" src="./../assets/instagram.png" alt="Instagram image">
-          <img class="socialim" src="./../assets/tiktok.png" alt="TikTok image">
-          <img class="socialim" src="./../assets/youtube.png" alt="YouTube image">
-          <img class="socialim" src="./../assets/twitter.png" alt="Twitter image">
+        <div>
+          <img src="./../assets/logo.png" class="log" alt="Logo">
+          <div class="flex aligncenter">
+            <img src="./../assets/email.png" class="emicon" alt="email icon">
+            <a href="mailto:support@recipewise.com"><span class="colw popno emsi emtext">support@recipewise.com</span></a>
+          </div>
+          <div class="flex aligncenter contacttop">
+            <img src="./../assets/phone.png" class="phicon" alt="phone icon">
+            <a href="tel:1300566466" class="ph"><span class="colw popno emsi">1300 566 466</span></a>
+          </div>
+          <div class="socialdiv flex spacebetween">
+            <img class="socialim" src="./../assets/facebook.png" alt="Facebook image">
+            <img class="socialim" src="./../assets/instagram.png" alt="Instagram image">
+            <img class="socialim" src="./../assets/tiktok.png" alt="TikTok image">
+            <img class="socialim" src="./../assets/youtube.png" alt="YouTube image">
+            <img class="socialim" src="./../assets/twitter.png" alt="Twitter image">
+          </div>
         </div>
       </div>
     </div>
