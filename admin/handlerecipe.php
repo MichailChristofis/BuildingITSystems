709367fe8ffbfbb31e4278bc30cb8faa670f07ca
s3923089 <90187172+MichailChristofis@users.noreply.github.com>
2022-10-27 22:34:15 +0300

    fixed up recipe upload for admins

--- admin/handlerecipe.php
+++ admin/handlerecipe.php
@@ -1,7 +1,9 @@
 <?php 
   include "./../pages/connect.php";
   if(isset($_POST["add-recipe"])){
+    var_dump($_FILES);
     $image=$_FILES["image"]["name"];
+    $fileTempName=$_FILES["image"]["tmp_name"];
     $name=$_POST["name"];
     $time=$_POST["time"];
     $rating=$_POST["rating"];
@@ -14,6 +16,9 @@
     $connection=$pdo->open();
     $recipe=$connection->prepare("INSERT INTO recipe (name, time, rating, calories, proteins, carbs, fat, sugar, fiber, img) VALUES (:a, :b, :c, :d, :e, :f, :g, :h, :i, :f)");
     $recipe->execute(["a"=>$name, "b"=>$time, "c"=>$rating, "d"=>$calories, "e"=>$proteins, "f"=>$carbs, "g"=>$fat, "h"=>$sugar, "i"=>$fiber, "f"=>$image]);
+    if(move_uploaded_file($fileTempName, "Uploads/".$image)){
+      echo "HI";
+    }
     if($recipe->rowCount()>0){
       $_SESSION["new-recipe"]="one recipe added";
     }
